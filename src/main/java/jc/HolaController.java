9941269927483f28d1d0473c2d05307e92fc05cd
jcoll <jcoll@hotmail.com>
2018-10-24 12:43:17 +0200

    Agregado method documentado

--- src/main/java/jc/HolaController.java
+++ src/main/java/jc/HolaController.java
@@ -2,13 +2,14 @@ package jc;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class HolaController {
     // Por defecto mapea a todos los tipos de request posibles: get, post, delete, etc...
-    @RequestMapping("/hola")
+    @RequestMapping("/hola" /*, method= RequestMethod.GET*/)
     public String sayHello() {
         return "<html><body><b>HOLA</b> </em>MUNDO!</em></body></html>";
     }
